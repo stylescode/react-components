8fd282070a8c2a7be69e2cb2e6ef32ea8538c6c8
Styles <stylesk11@gmail.com>
2024-02-05 11:57:03 -0800

    create components, use hook to alter state

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
 // TODO
+const { useState } = React;
+
+const GroceryListItem = (props) => {
+
+  const [hovered, setHovered] = useState(false);
+
+  let style = {
+    fontWeight: hovered ? 'bold': 'normal',
+  };
+
+  return (
+    <li style={style} onMouseOver={() => setHovered(true)} onMouseOut={() => setHovered(false)}>{props.item}</li>
+  )
+}
+
+const GroceryList = (props) => {
+  return (
+    <ul>
+      {props.groceryItems.map((item) => (
+        <GroceryListItem item={item} />
+      ))}
+    </ul>
+  )
+}
+
+ReactDOM.render(<GroceryList groceryItems={['Bacon', 'Bananas']} />, document.getElementById('app'));
+
